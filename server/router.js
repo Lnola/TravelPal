830679be091d643c46ed9b74048f248d57b2df43
Lnola <nola.luka@gmail.com>
2022-09-20 19:56:39 +0200

    Fix double /api/api routes

--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-router.use('/api/auth', require('./routes/refresh_tokens'));
-router.use('/api/users', require('./routes/users'));
-router.use('/api/cityLocations', require('./routes/city_locations'));
-router.use('/api/trips', require('./routes/trips'));
-router.use('/api/tripLocations', require('./routes/trip_locations'));
-router.use('/api/locations', require('./routes/locations'));
-router.use('/api/favorites', require('./routes/favorites'));
+router.use('/auth', require('./routes/refresh_tokens'));
+router.use('/users', require('./routes/users'));
+router.use('/cityLocations', require('./routes/city_locations'));
+router.use('/trips', require('./routes/trips'));
+router.use('/tripLocations', require('./routes/trip_locations'));
+router.use('/locations', require('./routes/locations'));
+router.use('/favorites', require('./routes/favorites'));
 
 module.exports = router;
