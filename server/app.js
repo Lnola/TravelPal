e7fa460100f0b7d6bb1349b7e50599c536b68ebf
Lnola <nola.luka@gmail.com>
2022-09-20 19:56:25 +0200

    Move db healthcheck to database/index

--- server/app.js
+++ server/app.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const sequelize = require('./database');
 const router = require('./router');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 4000;
-
-sequelize
-  .authenticate()
-  .then(() => console.log('Database connection established'))
-  .catch((err) => console.log('Database connection failed', err));
-
-app.get('/api', (req, res) => res.send('INDEX'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', router);
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/database/index.js
+++ server/database/index.js
@@ -8,4 +8,9 @@ const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
   dialect: DB_DIALECT,
 });
 
+sequelize
+  .authenticate()
+  .then(() => console.log('Database connection established'))
+  .catch((err) => console.log('Database connection failed', err));
+
 module.exports = sequelize;
