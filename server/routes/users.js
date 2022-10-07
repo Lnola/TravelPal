5018f688d31869418238bf9a7abb55f272a72bde
Lnola <nola.luka@gmail.com>
2022-10-07 13:35:47 +0200

    Update users to async await syntax

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,16 +3,14 @@ const User = require('../models/User');
 
 const path = '/users';
 
-router.get('/', (req, res) =>
-  User.findAll()
-    .then((users) => {
-      console.log(users);
-      res.send(users);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500);
-    })
-);
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.send(users);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = { path, router };
