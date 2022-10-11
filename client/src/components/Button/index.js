20c18a7f7860b5c9fe81de35f9f0b6f6d5056d89
Lnola <nola.luka@gmail.com>
2022-10-12 00:27:14 +0200

    Add basic button component

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class AuthForm extends Component {
+  render() {
+    const { type, disabled, onClick, label } = this.props;
+
+    return (
+      <button
+        className='pal__button'
+        type={type}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
+  }
+}
+
+export default AuthForm;
