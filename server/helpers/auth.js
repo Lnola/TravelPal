554ebe806b4c3308146ddef1ec652cd2b69ea541
Lnola <nola.luka@gmail.com>
2022-10-12 17:56:13 +0200

    Fix generate tokens function

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 const randtoken = require('rand-token');
+const { FORBIDDEN } = require('http-status');
+
 const RefreshToken = require('../models/RefreshToken');
+
 const HttpError = require('./httpError');
-const { UNAUTHORIZED, FORBIDDEN } = require('http-status');
 const errorMessages = require('./errorMessages');
 
 const { JWT_SECRET_KEY } = process.env;
@@ -19,34 +21,19 @@ const verifyToken = (req, res, next) => {
   next();
 };
 
-const sign = async (res, user, accessToken) => {
-  // Create refresh token
-  const refreshToken = randtoken.uid(50);
-
-  // Save refresh token to DB
-  try {
-    const userId = user.id;
-    const newRefreshToken = { userId, token: refreshToken };
-
-    await RefreshToken.create(newRefreshToken);
-    const response = { accessToken, refreshToken, userId };
-    return res.json(response);
-  } catch (err) {
-    return next(new HttpError(FORBIDDEN, errorMessages.LOGIN_ERROR));
-  }
-};
-
-const generateRefreshToken = (res, user, doesMatch, next) => {
-  if (!doesMatch)
-    return next(new HttpError(UNAUTHORIZED, errorMessages.LOGIN_ERROR));
-
-  // Create jwt token
+const generateTokens = async (user) => {
   const payload = { user };
   const secretKey = JWT_SECRET_KEY;
   const options = { expiresIn: '5m' };
-  jwt.sign(payload, secretKey, options, (_, accessToken) =>
-    sign(res, user, accessToken)
-  );
+  const userId = user.id;
+
+  const accessToken = jwt.sign(payload, secretKey, options);
+  const refreshToken = randtoken.uid(50);
+
+  const tokenDbEntry = { userId, token: refreshToken };
+  await RefreshToken.create(tokenDbEntry);
+
+  return { accessToken, refreshToken, userId };
 };
 
-module.exports = { verifyToken, generateRefreshToken };
+module.exports = { verifyToken, generateTokens };
