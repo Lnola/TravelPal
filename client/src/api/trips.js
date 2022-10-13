389f682061af0cc8f1eef81fff5e53af12b3edfe
Lnola <nola.luka@gmail.com>
2022-10-13 03:51:55 +0200

    Add interfaces for trip add and edit

--- client/src/api/trips.js
+++ client/src/api/trips.js
@@ -3,11 +3,25 @@ import { extractData } from './helpers/helpers';
 
 const urls = {
   root: '/trips',
+  get add() {
+    return this.root + '/add';
+  },
+  get edit() {
+    return this.root + '/edit';
+  },
 };
 
 const fetch = () => {
   return request.get(urls.root).then(extractData);
 };
 
-const api = { fetch };
+const add = (params) => {
+  return request.post(urls.add, params).then(extractData);
+};
+
+const edit = (params) => {
+  return request.post(urls.edit, params).then(extractData);
+};
+
+const api = { fetch, add, edit };
 export default api;
