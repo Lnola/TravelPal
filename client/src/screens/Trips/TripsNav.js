3b25ad3ba9b2c18a29d41dc35f33ba179b943359
Lnola <nola.luka@gmail.com>
2022-10-13 02:48:59 +0200

    Remove logout button component and replace it with icon button

--- client/src/screens/Trips/TripsNav.js
+++ client/src/screens/Trips/TripsNav.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 
-import Logout from './Logout';
+import Button from '../../components/Button';
+import ButtonVariants from '../../components/Button/variants';
 import PlusButton from '../../components/PlusButton';
 
+import { authorizedRequest } from '../../utils/authorizedRequest';
+import { clearLocalStorage } from '../../utils/storage';
+
+import LogoutIcon from '../../assets/logout.svg';
+
 class TripsNav extends Component {
+  handleLogout = () => {
+    authorizedRequest(
+      `/api/auth/delete/${window.localStorage.getItem('id')}`,
+      'delete'
+    )
+      .then(() => clearLocalStorage())
+      .catch((err) => console.log('Something went wrong', err));
+  };
+
   render() {
     const { toggleModal } = this.props;
 
     return (
       <nav className='trips__nav'>
         <section className='trips__nav--wrapper'>
-          <Logout />
+          <Button
+            variant={ButtonVariants.icon}
+            icon={LogoutIcon}
+            onClick={this.handleLogout}
+          />
           <PlusButton
             className='plus__button--trips'
             onToggleModal={toggleModal}
